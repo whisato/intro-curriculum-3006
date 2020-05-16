cfe6a1e8c44fa326a511114af5f209bb07b7584c
hi <o8o6o191219@yahoo.co.jp>
2020-05-16 14:59:54 +0900

    おみくじ実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,16 @@
 'use strict';
-module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+module.exports = function(robot){
+    robot.hear( /hello/i, function(msg){
+        const user_id = msg.message.user.id;
+        msg.send(`やあ！新しいゲームあるよ！おみくじって言ってみてよ,<@${user_id}>`);
+    } );
+
+    robot.hear(/おみくじ/i, function(msg){
+        const user_id = msg.message.user.id;
+        const lots = ['大吉','吉','中吉','末吉','凶','大凶'];
+        const lot = lots[Math.floor(Math.random() *
+            lots.length)];
+            msg.send(`${lot},<@${user_id}>`);
+    });
 };
+
